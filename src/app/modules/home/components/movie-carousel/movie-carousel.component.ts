fa8f45728ded36b92eabead0dd15a87cdcb5c3fe
mechztcc <dojathan@gmail.com>
2023-11-30 07:00:01 -0500

    feat: first idea to movie-carousel

---
+++ src/app/modules/home/components/movie-carousel/movie-carousel.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-movie-carousel',
+  templateUrl: './movie-carousel.component.html',
+  styleUrls: ['./movie-carousel.component.scss']
+})
+export class MovieCarouselComponent {
+
+
+  icons = {
+    left: faChevronLeft,
+    right: faChevronRight
+  }
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MovieCarouselComponent } from './components/movie-carousel/movie-carousel.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomePageComponent, MovieCarouselComponent],
+  imports: [CommonModule, HomeRoutingModule, FontAwesomeModule],
 })
-export class HomeModule { }
+export class HomeModule {}
